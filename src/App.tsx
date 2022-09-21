e91c371f33c128c7a32d7b7ce080cff03b6ba505
kevinPJdev <75366911+kevinPJdev@users.noreply.github.com>
2022-09-21 16:43:34 -0400

    navbar start

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import Navbar from './components/Navbar/Navbar';
+
 function App() {
   return (
     <div>
-      <h1>App Screen</h1>
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { BsBellFill } from "react-icons/bs";
+import { BiSearch } from "react-icons/bi";
 
 type Props = {}
 
 const Navbar = (props: Props) => {
-  return (
-    <div>Navbar</div>
+
+  const [navBg, setNavBg] = useState<boolean>(false);
+
+  //Change the navbar bg color if user scrolls
+  window.addEventListener("scroll", () => {
+    if(window.scrollY >= 80) setNavBg(true);
+    else setNavBg(false);
+  });
+
+  return (  
+      <nav className= {navBg? "bg-transparent fixed m-auto w-full overflow-hidden py-2 flex align-center": "bg-black fixed m-auto w-full overflow-hidden py-2 flex align-center"}>
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+          alt="MyFlix_logo"
+          className="h-8 w-18 mx-12"
+        />
+
+        <aside className=''>
+          <BsBellFill className=""/>
+          <BiSearch />
+        </aside>
+      </nav>
   )
 }
 
