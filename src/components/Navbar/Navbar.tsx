61e8020a694552725a16e0f8ee5ee645eaae7c34
kevinPJdev <75366911+kevinPJdev@users.noreply.github.com>
2022-09-22 01:31:49 -0400

    navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,33 +1,48 @@
-import React, {useState} from 'react'
 import { BsBellFill } from "react-icons/bs";
 import { BiSearch } from "react-icons/bi";
 
-type Props = {}
+import "./Navbar.css";
+import { useState } from "react";
 
-const Navbar = (props: Props) => {
+export function Navbar(): JSX.Element {
+  const [navbar, setNavbar] = useState<boolean>(false);
 
-  const [navBg, setNavBg] = useState<boolean>(false);
+  const changeBackground = () => {
+    if (window.scrollY >= 50) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
 
-  //Change the navbar bg color if user scrolls
-  window.addEventListener("scroll", () => {
-    if(window.scrollY >= 80) setNavBg(true);
-    else setNavBg(false);
-  });
+  window.addEventListener("scroll", changeBackground);
+  return (
+    <nav className={navbar ? "nav active" : "nav"}>
+      <img
+        tabIndex={1}
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+        alt="Netflix logo"
+        className="nav__logo"
+      />
 
-  return (  
-      <nav className= {navBg? "bg-transparent fixed m-auto w-full overflow-hidden py-2 flex align-center": "bg-black fixed m-auto w-full overflow-hidden py-2 flex align-center"}>
-        <img 
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
-          alt="MyFlix_logo"
-          className="h-8 w-18 mx-12"
+      <img
+        tabIndex={1}
+        src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-N-Symbol-logo-red-transparent-RGB-png.png"
+        alt="Netflix mobile logo"
+        className="nav__log--mobile"
+      />
+
+      <aside className="nav__wrapper">
+        <BiSearch className="wrapper__icon" />
+        <BsBellFill className="wrapper__icon" />
+
+        <img
+          tabIndex={1}
+          src="http://zoeice.com/assets/img/uploads/profile.png"
+          alt="Avatar"
+          className="wrapper__avatar"
         />
-
-        <aside className=''>
-          <BsBellFill className=""/>
-          <BiSearch />
-        </aside>
-      </nav>
-  )
+      </aside>
+    </nav>
+  );
 }
-
-export default Navbar
